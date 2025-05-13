a8be41dfc67d7292626b9cb1288f8173b2d3965f
Shalini <shalini.hansaka@gmail.com>
2025-05-13 02:36:00 +0200

    refactor: styles

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './header.module.scss';
+
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src='' alt='Logo' />
+    </header>
+  )
+}
+
+export default Header
